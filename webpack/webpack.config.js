be296509c584188885bc7ecf4b99f02a0b7e5289
lasoeurdupape <45121681+jmaudouin@users.noreply.github.com>
2024-07-31 20:03:48 +0200

    dev

---
+++ webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../assets/bundle'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            }
+        ],
+    }
+};
